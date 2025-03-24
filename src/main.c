8e8642a2a9ed41850638575734532bcc0acd9e10
Mohammad Afaneh <mafaneh@gmail.com>
2025-03-24 03:20:52 -0400

    Initial commit with functionality for changing Playbulb colors and reading battery level

--- src/main.c
+++ src/main.c
@@ -19,58 +19,248 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/sys/byteorder.h>
 
-static void start_scan(void);
+#include <dk_buttons_and_leds.h>
 
+// Important defines
+#define NAME_LEN      30
+#define PERIPHERAL_NAME "PLAYBULB CANDLE II"
+
+// Color Settings
+#define COLOR_COUNT 4
+#define COLOR_WHITE 0x000000FF
+#define COLOR_RED   0x0000FF00
+#define COLOR_GREEN 0x00FF0000
+#define COLOR_BLUE  0xFF000000
+
+#define COLOR_STRING_MAX_LENGTH 6
+
+typedef struct {
+	uint32_t color_value;
+	char color_name[COLOR_STRING_MAX_LENGTH];
+
+} color_setting_t;
+
+static color_setting_t color_array[COLOR_COUNT] =
+{
+	{COLOR_WHITE, "White"},
+	{COLOR_RED, "Red"},
+	{COLOR_GREEN, "Green"},
+	{COLOR_BLUE, "Blue"} };
+
+#define BT_UUID_COLOR_SETTING BT_UUID_DECLARE_16(0xFFFC)
+
+// Button Action Assignments
+// - Button 1 ==> Rotate Colors
+// - Button 2 ==> Read Battery Level
+// - Button 3 ==> Update Connection Parameters
+// - Button 4 ==> Disconnect from device
+#define BUTTON_COLOR         DK_BTN1_MSK
+#define BUTTON_BATTERY_LEVEL DK_BTN2_MSK
+#define BUTTON_CONN_PARAMS   DK_BTN3_MSK
+#define BUTTON_DISCONNECT    DK_BTN4_MSK
+
+// Data Types
+typedef struct adv_data
+{
+    // Device Name
+	uint8_t length;
+    char name[NAME_LEN];
+
+} custom_adv_data_t;
+
+// Important global variables
+static K_SEM_DEFINE(sem_connected, 0, 1);
+static K_SEM_DEFINE(sem_discovered, 0, 1);
+static K_SEM_DEFINE(sem_written, 0, 1);
+static K_SEM_DEFINE(sem_read_operation, 0, 1);
+static K_SEM_DEFINE(sem_disconnected, 0, 1);
+
+static uint16_t color_attr_handle;
+static uint16_t battery_level_value_handle;
+static uint32_t current_color_index = 0; // Start with White
 static struct bt_conn *default_conn;
+
+static struct bt_uuid_16 discover_uuid = BT_UUID_INIT_16(0);
+static struct bt_gatt_discover_params discover_params;
+static struct bt_gatt_subscribe_params subscribe_params;
+static struct bt_gatt_read_params read_params;
+
+// Function prototypes
+static void start_scan(void);
+static int change_color(void);
+
+// Functions
+
+static bool data_cb(struct bt_data *data, void *user_data)
+{
+    custom_adv_data_t *adv_data_struct = user_data;
+    uint8_t len;
+
+    switch (data->type) {
+    case BT_DATA_NAME_SHORTENED:
+    case BT_DATA_NAME_COMPLETE:
+        len = MIN(data->data_len, NAME_LEN - 1);
+        memcpy(adv_data_struct->name, data->data, len);
+        adv_data_struct->name[len] = '\0';
+		adv_data_struct->length = len;
+        return false;
+    default:
+        return true;
+    }
+}
 
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
 {
 	char addr_str[BT_ADDR_LE_STR_LEN];
+	custom_adv_data_t device_ad_data;
 	int err;
 
 	if (default_conn) {
 		return;
 	}
 
-	/* We're only interested in connectable events */
-	if (type != BT_GAP_ADV_TYPE_ADV_IND &&
-	    type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
+	if (type != BT_GAP_ADV_TYPE_ADV_IND && type != BT_GAP_ADV_TYPE_SCAN_RSP)
+	{
 		return;
 	}
+
+    (void)memset(&device_ad_data, 0, sizeof(custom_adv_data_t));
+    bt_data_parse(ad, data_cb, &device_ad_data);
 
 	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
 
-	/* connect only to devices in close proximity */
-	if (rssi < -50) {
+	// Only log and care about devices with a non-empty device name
+	if (device_ad_data.length == 0)
+	{
 		return;
 	}
 
-	if (bt_le_scan_stop()) {
-		return;
+	printk("Device found [%s]: %s with name [%d]: |%s| (RSSI %d)\n", 
+			type == BT_GAP_ADV_TYPE_SCAN_RSP ? "Scan Response":"Regular Advertisement",
+			addr_str,
+			device_ad_data.length,
+			device_ad_data.name,
+			rssi);
+
+	if (strncmp(PERIPHERAL_NAME, device_ad_data.name, strlen(PERIPHERAL_NAME)) == 0)
+	{
+		if (bt_le_scan_stop()) {
+			return;
+		}
+
+		err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
+						BT_LE_CONN_PARAM_DEFAULT, &default_conn);
+			if (err) {
+				printk("Create conn to %s failed (%d)\n", addr_str, err);
+				start_scan();
+			}
+	}	
+}
+
+static uint8_t notify_func(struct bt_conn *conn,
+			   struct bt_gatt_subscribe_params *params,
+			   const void *data, uint16_t length)
+{
+	uint8_t *battery_level = ((uint8_t *)data);
+
+	if (!data) {
+		printk("[UNSUBSCRIBED]\n");
+		params->value_handle = 0U;
+		return BT_GATT_ITER_STOP;
 	}
 
-	err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
-				BT_LE_CONN_PARAM_DEFAULT, &default_conn);
-	if (err) {
-		printk("Create conn to %s failed (%d)\n", addr_str, err);
-		start_scan();
+	printk("Received notification for Battery Level (%u): %u%%\n", length, *battery_level);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static uint8_t discover_func(struct bt_conn *conn,
+			     const struct bt_gatt_attr *attr,
+			     struct bt_gatt_discover_params *params)
+{
+	int err;
+
+	if (!attr) {
+		printk("Discover complete\n");
+		(void)memset(params, 0, sizeof(*params));
+		return BT_GATT_ITER_STOP;
 	}
+
+	printk("Discovered [ATTRIBUTE] with handle %u\n", attr->handle);
+
+	// Discovered the Color Characteristic
+	if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_COLOR_SETTING)) {
+
+		printk("Discovery of Color Setting Characteristic Successful\n");
+		// color_attr_handle = ((struct bt_gatt_chrc *)attr->user_data)->value_handle;
+		color_attr_handle = bt_gatt_attr_value_handle(attr);
+		printk("Color Setting Characteristic Handle = %u\n", color_attr_handle);
+
+		// Move on to discovering the Battery Level Characteristic
+		memcpy(&discover_uuid, BT_UUID_BAS_BATTERY_LEVEL, sizeof(discover_uuid));
+		discover_params.uuid = &discover_uuid.uuid;
+		discover_params.start_handle = attr->handle + 1;
+		discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+
+		err = bt_gatt_discover(conn, &discover_params);
+		if (err) {
+			printk("Discover failed (err %d)\n", err);
+		}
+	}
+	// Discovered the Battery Level Characteristic
+	else if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_BAS_BATTERY_LEVEL)) {
+
+		printk("Discovery of Battery Level Characteristic Successful\n");
+		// battery_level_value_handle = ((struct bt_gatt_chrc *)attr->user_data)->value_handle;
+		battery_level_value_handle = bt_gatt_attr_value_handle(attr);
+		printk("Battery Level Characteristic Handle = %u\n", battery_level_value_handle);
+		
+		// Move on to discovering the Battery Level Characteristic CCCD (to enable notifications)
+		memcpy(&discover_uuid, BT_UUID_GATT_CCC, sizeof(discover_uuid));
+		discover_params.uuid = &discover_uuid.uuid;
+		discover_params.start_handle = attr->handle + 2;
+		discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+		subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
+
+		err = bt_gatt_discover(conn, &discover_params);
+		if (err) {
+			printk("Discover failed (err %d)\n", err);
+		}
+	}
+	else
+	{
+		printk("Discovery of Battery Level Characteristic CCCD Successful. Subscribing to notifications now.\n");
+
+		subscribe_params.notify = notify_func;
+		subscribe_params.value = BT_GATT_CCC_NOTIFY;
+		subscribe_params.ccc_handle = attr->handle;
+
+		err = bt_gatt_subscribe(conn, &subscribe_params);
+		if (err && err != -EALREADY) {
+			printk("Subscribe failed (err %d)\n", err);
+		} else {
+			printk("[SUBSCRIBED]\n");
+		}
+
+		k_sem_give(&sem_discovered);
+		return BT_GATT_ITER_STOP;
+	}
+
+	return BT_GATT_ITER_STOP;
 }
 
 static void start_scan(void)
 {
 	int err;
 
-	/* This demo doesn't require active scan */
-	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, device_found);
+	err = bt_le_scan_start(BT_LE_SCAN_ACTIVE, device_found);
 	if (err) {
 		printk("Scanning failed to start (err %d)\n", err);
 		return;
 	}
 
-	printk("Scanning successfully started\n");
+	printk("Scanning successfully started\n ==> Will only be scanning devices with a NON-EMPTY device name <==\n");
 }
 
 static void connected(struct bt_conn *conn, uint8_t err)
@@ -95,7 +285,7 @@ static void connected(struct bt_conn *conn, uint8_t err)
 
 	printk("Connected: %s\n", addr);
 
-	bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+	//bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
@@ -112,18 +302,100 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 	bt_conn_unref(default_conn);
 	default_conn = NULL;
+	k_sem_give(&sem_disconnected);
+}
 
-	start_scan();
+void remote_info_available_cb(struct bt_conn *conn, struct bt_conn_remote_info *remote_info)
+{
+	k_sem_give(&sem_connected);
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = connected,
 	.disconnected = disconnected,
+    .remote_info_available = remote_info_available_cb
 };
+
+static uint8_t battery_read_func(struct bt_conn *conn, uint8_t err,
+	struct bt_gatt_read_params *params,
+	const void *data, uint16_t length)
+{
+	printk("Battery Level = %u%% 0x%04X\n", *((uint8_t *)data), *((uint16_t *)data));
+
+	return BT_GATT_ITER_STOP;
+}
+
+static int read_battery_level(void)
+{
+	int err;
+
+	read_params.func = battery_read_func;
+	read_params.handle_count  = 1;
+	read_params.single.handle = battery_level_value_handle;
+	read_params.single.offset = 0;
+
+	err = bt_gatt_read(default_conn, &read_params);
+	if (err)
+	{
+		printk("Read not successful!\n");
+	}
+
+	return err;
+}
+
+static void button_state_changed(uint32_t button_state, uint32_t has_changed)
+{
+    if ((has_changed & BUTTON_COLOR) && (button_state & BUTTON_COLOR))
+	{	
+		printk("Changing color to next one in array\n");
+		current_color_index = (current_color_index + 1) % COLOR_COUNT;
+		change_color();
+    }
+	if ((has_changed & BUTTON_BATTERY_LEVEL) && (button_state & BUTTON_BATTERY_LEVEL))
+	{
+		printk("Reading the Battery Level\n");
+		read_battery_level();
+	}
+}
+
+static int init_buttons(void)
+{
+    return dk_buttons_init(button_state_changed);
+}
+
+static int change_color(void)
+{
+	int err;
+
+	printk("Setting color to: %s\n", color_array[current_color_index].color_name);
+
+	err = bt_gatt_write_without_response(default_conn,
+										 color_attr_handle,
+										 &(color_array[current_color_index].color_value),
+										 sizeof(uint32_t),
+										 false);
+	if (err) {
+		printk("Write failed (err %d)\n", err);
+	}
+	else // Write sent successfully
+	{
+		printk("Write without response sent successfully.\n");
+	}
+
+	return err;
+}
+
 
 int main(void)
 {
 	int err;
+
+	err = init_buttons();
+	if (err)
+	{
+		printk("Cannot init buttons (err: %d)\n", err);
+		return 0;
+	}
 
 	err = bt_enable(NULL);
 	if (err) {
@@ -133,6 +405,57 @@ int main(void)
 
 	printk("Bluetooth initialized\n");
 
-	start_scan();
+	while (1)
+	{
+		// Continuously scan
+		start_scan();
+
+		// Wait for a connection to happen
+		k_sem_take(&sem_connected, K_FOREVER);
+
+		// Once connected, we discover the GATT database of the connected Peripheral
+		memcpy(&discover_uuid, BT_UUID_COLOR_SETTING, sizeof(discover_uuid));
+		discover_params.uuid = &discover_uuid.uuid;
+		discover_params.func = discover_func;
+		discover_params.start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
+		discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
+		discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+		err = bt_gatt_discover(default_conn, &discover_params);
+		if (err) {
+			printk("Discovery failed (err %d)\n", err);
+		}
+		else // Discovery started successfully
+		{
+			printk("Discovery started\n");
+
+			err = k_sem_take(&sem_discovered, K_SECONDS(10));
+			if (err) {
+				printk("Timed out during GATT discovery\n");
+			}
+			else // Discovery completed successfully
+			{
+				printk("Discovered the device's characteristics.\n");
+
+				// Stay in this state until a disconnect occurs.
+				printk("Wait here for user button presses as long as we're still connected...\n");
+
+				k_sem_take(&sem_disconnected,  K_FOREVER);
+			}
+		}
+
+		if (err)
+		{
+			// In case of an error, disconnect from the device
+			err = bt_conn_disconnect(default_conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+			if (err) {
+				printk("Failed to disconnect (err %d)\n", err);
+				return 0;
+			}
+
+			k_sem_take(&sem_disconnected,  K_SECONDS(30));	
+			printk("Disconnected. Going back to scanning again...\n");
+		}
+	}
+	
 	return 0;
 }
